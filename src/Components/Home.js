6293c6865ccee4c39a0e12e48383b2c20d4b9904
Dhruv Sahu <82047955+Dhruv-Sahu@users.noreply.github.com>
2023-07-17 18:37:15 +0530

    npm update

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -15,7 +15,6 @@ import './Home.css'
 
 
 const Home = () => {
-
   const handleBookNowClick = () => {
     console.log("book now clicked")
     ReactGa.event({
@@ -24,17 +23,13 @@ const Home = () => {
       label: "Book Now Button from Hero Page is clicked"
     });
   };
-
   return (
     <>
       <section id="hero">
         <div className="hero-container">
           <div id="heroCarousel" data-bs-interval="5000" className="carousel slide carousel-fade" data-bs-ride="carousel">
-
             <ol className="carousel-indicators" id="hero-carousel-indicators"></ol>
-
             <div className="carousel-inner" role="listbox">
-
               <div className="carousel-item active">
                 <video className='' style={{
                   position: "relative",
@@ -56,35 +51,7 @@ const Home = () => {
                   </div>
                 </div>
               </div>
-              <div className="carousel-item"
-              >
-                <div className="carousel-container">
-                  <div className="carousel-content">
-                    <h2 className="animate__animated animate__fadeInDown">Lorem Ipsum Dolor</h2>
-                    <p className="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
-                    <div>
-                      <a href="#menu" className="btn-menu animate__animated animate__fadeInUp scrollto">Our Menu</a>
-                      <a href="#book-an-aerem" className="btn-book animate__animated animate__fadeInUp scrollto">Book a Table</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="carousel-item"
-              >
-                <div className="carousel-container">
-                  <div className="carousel-content">
-                    <h2 className="animate__animated animate__fadeInDown">Sequi ea ut et est quaerat</h2>
-                    <p className="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
-                    <div>
-                      <a href="#menu" className="btn-menu animate__animated animate__fadeInUp scrollto">Our Menu</a>
-                      <a href="#book-a-table" className="btn-book animate__animated animate__fadeInUp scrollto">Book a Table</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
             </div>
-
           </div>
         </div>
       </section>
